167a4a0db65ab3372baadef392e872bba9d3f90f
Pier4413 <pierre.delmas@delmasweb.net>
2023-12-30 18:07:10 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='logger',
     packages=['logger'],
     description='A simple wrapper for logging in Python',
-    version='1.0.7',  # updated
-    url='',
+    version='1.0.8',  # updated
+    url='https://github.com/Pier4413/PythonSimpleLog',
     author='Panda',
     author_email='panda@delmasweb.net',
     keywords=['logger', 'logs', 'logging']
