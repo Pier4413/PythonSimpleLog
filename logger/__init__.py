654e9db78b1fc0b096e5f08fdc8c693019deb08c
Pier4413 <pierre.delmas@delmasweb.net>
2025-05-20 18:49:10 +0000

    New paradigm for metadata

--- logger/__init__.py
+++ logger/__init__.py
@@ -7,11 +7,27 @@ import logging
 import logging.handlers
 from typing import Optional
 
+from theophanie_models.life import Life as LifeMessage
+
 __authors__ = ["Panda <panda@delmasweb.net>"]
 __short_licence_name__ = "No licence"
 __long_licence_text__ = ""
 __version__ = "1.0.0"
 __name__ = "Logger"
+
+def create_life_message() -> LifeMessage:
+
+  """
+    Create the life message in the format of models
+
+    :rtype: LifeMessage
+  """
+  authors_part = LifeMessage(text="Authors", value=str(__authors__))
+  licence_part = LifeMessage(text="Licence", value=__short_licence_name__)
+  version_part = LifeMessage(text="Version", value=__version__)
+  life_message = LifeMessage(text=__name__, childs=[version_part, authors_part, licence_part])
+
+  return life_message
 
 class Logger(object):
   """
